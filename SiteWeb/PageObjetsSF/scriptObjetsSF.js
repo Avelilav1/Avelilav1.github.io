99cf4ce8f44eba5a5d6c9df86671f0110fa769e6
Avelilav1 <aram.orizet@gmail.com>
2026-09-05 12:12:06 +0200

    pleins de changements et d'optimisation trop stylés

--- SiteWeb/PageObjetsSF/scriptObjetsSF.js
+++
@@ -1,5 +0,0 @@
-console.log("test")
-
-function afficherSF() {
-    console.log("afficher sf");
-}
--- SiteWeb/script.js
+++ SiteWeb/script.js
@@ -1,8 +1,8 @@
 console.log("test reussi ")
 
 let descriptionStationSpatiale = "This is an ultra-realistic spaceship that took many hours of hardwork and passion. We're hoping that you'll like this model, feel free to send some love to Avelilav1 who coded this site and to Mineki who created this 3D model."
-let lienStationSpatiale = "assets/scifiship_final.blend";
-let tablimg = ["assets/imageTest.webp"];
+let lienStationSpatiale = "../../assets/scifiship_final.blend";
+let tablimg = ["../../assets/imageTest.webp"];
 
 function afficherSF() {
     let elem = document.getElementById("objetsSF");
@@ -54,34 +54,29 @@ function afficherPageImg(img) {
     }
 
 
-    let description = document.getElementById("descriptionObjet");
-    let lien = document.getElementById("lienSS");
-    console.log(lien);
-    console.log(lienStationSpatiale);
 
+    if (img.alt == "1") {
+        tablimg = ["../../assets/scifiship_image1.png", "../../assets/scifiship_image2.png"];
+        let i = 0;
+        let button = document.getElementById("buttonS");
+        let imgAct = document.getElementById("imageConcernee");
+        button.addEventListener("click", function () {
+            i = i + 1;
+            if (i >= tablimg.length) {
+                i = 0;
+            }
+            imgAct.src = tablimg[i];
+        })
 
-        if (img.alt == "1") {
-            tablimg = ["assets/scifiship_image1.png", "assets/scifiship_image2.png"];
-            let i = 0;
-            let button = document.getElementById("buttonS");
-            let imgAct = document.getElementById("imageConcernee");
-            button.addEventListener("click", function () {
-                i = i + 1;
-                if (i >= tablimg.length) {
-                    i = 0;
-                }
-                imgAct.src = tablimg[i];
-            })
-
-            description.textContent = descriptionStationSpatiale;
-            lien.href = lienStationSpatiale;
-            console.log(lien.href);
-        } else {
-            lien.href = "#";
-            description.textContent = "";
-            tablimg = [""];
-        }
+        description.textContent = descriptionStationSpatiale;
+        lien.href = lienStationSpatiale;
+        console.log(lien.href);
+    } else {
+        lien.href = "#";
+        description.textContent = "";
+        tablimg = [""];
     }
+}
 
 function retour() {
     pageObjet.style.display = "none";
@@ -90,3 +85,21 @@ function retour() {
 }
 
 
+//-------------------------------------
+
+let titrePage = document.getElementById("titre");
+
+if (titrePage.textContent == "SciFiShip") {
+    console.log("ca marche");
+    tablimg = ["../../assets/scifiship_image1.png", "../../assets/scifiship_image2.png"];
+    let i = 0;
+    let button = document.getElementById("buttonS");
+    let imgAct = document.getElementById("imageConcernee");
+    button.addEventListener("click", function () {
+        i = i + 1;
+        if (i >= tablimg.length) {
+            i = 0;
+        }
+        imgAct.src = tablimg[i];
+    })
+}
